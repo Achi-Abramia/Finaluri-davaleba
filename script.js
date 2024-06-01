42a431c59021133e62e4e60722ea1da76ec6b185
Achi-Abramia <163457483+Achi-Abramia@users.noreply.github.com>
2024-06-01 18:17:53 +0400

    added responsive for main slider

--- script.js
+++ script.js
@@ -39,22 +39,3 @@
         }
         moveSlide()
     }
-// const slidePictures = document.getElementsByClassName(".slide_pictures")
-// const images = document.querySelectorAll(".slide_pictures img")
-
-// let slideIndex = 0
-// setInterval(slideShow, 1000)
-
-// function slideShow() {
-//     slideIndex++
-//     if (slideIndex >= images.length) {
-//         slideIndex = 0
-//     }
-//     nextSlide()
-// }
-
-// function nextSlide() {
-//     const next = -slideIndex * 100
-//     slidePictures.style.transform = `transalteX(${next})`
-// }
-
