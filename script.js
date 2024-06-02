1873544f49122f314fd169b58bbd1269fe42e8d4
Achi-Abramia <163457483+Achi-Abramia@users.noreply.github.com>
2024-06-03 00:43:34 +0400

    added my info section with responsive

--- script.js
+++ script.js
@@ -39,3 +39,54 @@
         }
         moveSlide()
     }
+
+
+    function move(elementId, elementWidth) {
+        var elem = document.getElementById(elementId)
+        var width = 1
+        var id = setInterval(frame, 20)
+        function frame() {
+            if (width >= elementWidth) {
+                clearInterval(id)
+            } else {
+                width++
+                elem.style.width = width + "%"
+            }
+        }
+    }
+
+    let scrollCount = 0
+
+    window.addEventListener("scroll", function() {
+        scrollCount++
+        if (scrollCount === 30) {
+            move("html_bar", 100)
+            move("css_bar", 80)
+            move("javascript_bar", 50)
+            move("github_bar", 70) 
+        }
+    })
+
+
+    // function move() {
+    //     var elem = document.getElementById("myBar");
+    //     var width = 1;
+    //     var id = setInterval(frame, 40);
+    //     function frame() {
+    //       if (width >= 80) {
+    //         clearInterval(id);
+    //       } else {
+    //         width++;
+    //         elem.style.width = width + '%';
+    //       }
+    //     }
+    //   }
+
+    //   let scrollCount = 0;
+
+    //     document.addEventListener('scroll', function() {
+    //         scrollCount++;
+    //         if (scrollCount === 10) {
+    //             move();
+    //         }
+    //     });
