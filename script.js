8f4507b30e1d6a4ddc6b33f7a2671a3e918505e9
Achi-Abramia <163457483+Achi-Abramia@users.noreply.github.com>
2024-06-10 09:55:26 +0000

    edited javascript for form submit and validation

--- script.js
+++ script.js
@@ -182,113 +182,199 @@
     const formContainer = document.querySelector(".contact_form_bars_container")
     formContainer.appendChild(statusField)
 
-    contactForm.addEventListener("submit", (e) => {
-        e.preventDefault()
+    contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const name = document.getElementById("your_name")
-        const email = document.getElementById("email")
-        const website = document.getElementById("website")
-        const message = document.getElementById("message")
+    const name = document.getElementById("your_name");
+    const email = document.getElementById("email");
+    const website = document.getElementById("website");
+    const message = document.getElementById("message");
+
+         try {
+           if (name.value.length < 4 || name.value.length > 15) {
+             name.style.border = "4px solid #E93656";
+             throw new Error("Name should contain from 4 to 15 letters");
+           } else {
+             name.style.border = "4px solid green";
+           }
+       
+           const validateEmail = (mail) => {
+             return String(mail)
+               .toLowerCase()
+               .match(
+                 /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+               );
+           };
+       
+           const isValidateEmail = validateEmail(email.value);
+           if (!isValidateEmail) {
+             email.style.border = "4px solid #E93656";
+             throw new Error("Email is not valid");
+           } else {
+             email.style.border = "4px solid green";
+           }
+       
+           const validateWebsite = (web) => {
+             return String(web)
+               .toLowerCase()
+               .match(
+                 /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
+               );
+           };
+       
+           const isValidateWebsite = validateWebsite(website.value);
+           if (!isValidateWebsite) {
+             website.style.border = "4px solid #E93656";
+             throw new Error("Website is not valid");
+           } else {
+             website.style.border = "4px solid green";
+           }
+       
+           if (message.value.length === 0 || message.value.length > 400) {
+             message.style.border = "4px solid #E93656";
+             throw new Error("Message should contain from 1 to 400 letters");
+           } else {
+             message.style.border = "4px solid green";
+           }
+         } catch (error) {
+           statusField.textContent = error.message;
+         }
+       
+         let xhr = new XMLHttpRequest();
+         xhr.open("POST", "https://borjomi.loremipsum.ge/api/send-message", true);
+         xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+       
+         xhr.onreadystatechange = function () {
+           if (xhr.readyState === XMLHttpRequest.DONE) {
+             if (xhr.status >= 200 && xhr.status < 300) {
+               modal_container.classList.add("show");
+             }
+           }
+         };
+       
+         let data = {
+           name: name.value,
+           email: email.value,
+           website: website.value,
+           message: message.value,
+         };
+       
+         xhr.send(JSON.stringify(data));
+});
+
+   const modal_container = document.getElementById("modal_container");
+   const close = document.getElementById("close_modal");
+
+   close.addEventListener("click", () => {
+   modal_container.classList.remove("show");
+   location.reload();
+});
+
+    // contactForm.addEventListener("submit", (e) => {
+    //     e.preventDefault()
+
+    //     const name = document.getElementById("your_name")
+    //     const email = document.getElementById("email")
+    //     const website = document.getElementById("website")
+    //     const message = document.getElementById("message")
 
         
 
-        try {
-            if (name.value.length < 4 || name.value.length > 15) {
-                name.style.border = "4px solid #E93656"
-                throw new Error("Name should contain from 4 to 15 letters")
-            } else {
-                name.style.border = "4px solid green"
-            }
+    //     try {
+    //         if (name.value.length < 4 || name.value.length > 15) {
+    //             name.style.border = "4px solid #E93656"
+    //             throw new Error("Name should contain from 4 to 15 letters")
+    //         } else {
+    //             name.style.border = "4px solid green"
+    //         }
 
-            const validateEmail = (mail) => {
-                return String(mail)
-                  .toLowerCase()
-                  .match(  
-                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
-              }
+    //         const validateEmail = (mail) => {
+    //             return String(mail)
+    //               .toLowerCase()
+    //               .match(  
+    //                 /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    //           }
     
-              const isValidateEmail = validateEmail(email.value)
-              if (!isValidateEmail) {
-                email.style.border = "4px solid #E93656"
-                throw new Error("Email is not valid")
-              } else {
-                email.style.border = "4px solid green"
-              }
+    //           const isValidateEmail = validateEmail(email.value)
+    //           if (!isValidateEmail) {
+    //             email.style.border = "4px solid #E93656"
+    //             throw new Error("Email is not valid")
+    //           } else {
+    //             email.style.border = "4px solid green"
+    //           }
 
-              const validateWebsite = (web) => {
-                return String(web)
-                  .toLowerCase()
-                  .match(/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/)
-              }
+    //           const validateWebsite = (web) => {
+    //             return String(web)
+    //               .toLowerCase()
+    //               .match(/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/)
+    //           }
 
-              const isValidateWebsite = validateWebsite(website.value)
-              if (!isValidateWebsite) {
-                website.style.border = "4px solid #E93656"
-                throw new Error("Website is not valid")
-              } else {
-                website.style.border = "4px solid green"
-              }
+    //           const isValidateWebsite = validateWebsite(website.value)
+    //           if (!isValidateWebsite) {
+    //             website.style.border = "4px solid #E93656"
+    //             throw new Error("Website is not valid")
+    //           } else {
+    //             website.style.border = "4px solid green"
+    //           }
 
-              if (message.value === "" || message.value.length > 400) {
-                message.style.border = "4px solid #E93656"
-                throw new Error("Message should contain from 1 to 400 letters")
-              } else {
-                message.style.border = "4px solid green"
-              }
+    //           if (message.value === "" || message.value.length > 400) {
+    //             message.style.border = "4px solid #E93656"
+    //             throw new Error("Message should contain from 1 to 400 letters")
+    //           } else {
+    //             message.style.border = "4px solid green"
+    //           }
               
-              if (name.style.border === "4px solid green" && email.style.border === "4px solid green" && website.style.border === "4px solid green" && message.style.border === "4px solid green") {
-                statusField.textContent = "Thank you, information sent successfully"
-                statusField.style.color = "green"
-                setTimeout(() => {
-                    location.reload()
-                }, 5000)
-              } else {
-                statusField.textContent = "An error occurred. please try again later"
-              }
+    //           if (name.style.border === "4px solid green" && email.style.border === "4px solid green" && website.style.border === "4px solid green" && message.style.border === "4px solid green") {
+    //             statusField.textContent = "Thank you, information sent successfully"
+    //             statusField.style.color = "green"
+    //             setTimeout(() => {
+    //                 location.reload()
+    //             }, 5000)
+    //           } else {
+    //             statusField.textContent = "An error occurred. please try again later"
+    //           }
 
-        } catch (error) {
-            statusField.textContent = error.message
-        }
+    //     } catch (error) {
+    //         statusField.textContent = error.message
+    //     }
 
-        // const open = document.getElementsByClassName("send_message")
-        // const modal_container = document.getElementById("modal_container");
-        // const close = document.getElementById("close");
+    //     // const open = document.getElementsByClassName("send_message")
+    //     // const modal_container = document.getElementById("modal_container");
+    //     // const close = document.getElementById("close");
 
-        //     open.addEventListener("click", () => {
-        //     modal_container.classList.add("show")
-        // })
+    //     //     open.addEventListener("click", () => {
+    //     //     modal_container.classList.add("show")
+    //     // })
 
-        //     close.addEventListener("click", () => {
-        //     modal_container.classList.remove("show")
-        // })
+    //     //     close.addEventListener("click", () => {
+    //     //     modal_container.classList.remove("show")
+    //     // })
 
-            const formContent = new FormData(contactForm)
-            const info = Object.fromEntries(formContent)
+    //         const formContent = new FormData(contactForm)
+    //         const info = Object.fromEntries(formContent)
 
-            fetch("https://jsonplaceholder.typicode.com/posts", {
-                method:"POST",
-                body: JSON.stringify(info)
-            })
-                .then(res => res.json())
-                .then(data => console.log(data))
-                .then(error => console.log(error))
+    //         fetch("https://jsonplaceholder.typicode.com/posts", {
+    //             method:"POST",
+    //             body: JSON.stringify(info)
+    //         })
+    //             .then(res => res.json())
+    //             .then(data => console.log(data))
+    //             .then(error => console.log(error))
 
 
-                // ნამდვილად ძალიან ვეცადე მაგრამ არ გამომივიდა
+    //             // ნამდვილად ძალიან ვეცადე მაგრამ არ გამომივიდა
 
-            let xhr = new XMLHttpRequest()
-            xhr.open("POST", "https://jsonplaceholder.typicode.com/posts", true)
-            xhr.setRequestHeader("content-type", "application/json;charset=UTF-8")
-            xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status >= 200 && xhr.status < 300) {
-                alert("message sent")
-            } else if (xhr.readyState === XMLHttpRequest.DONE) {
-                alert("error")
-            }
-        }
-        
-
-    })
+    //         let xhr = new XMLHttpRequest()
+    //         xhr.open("POST", "https://jsonplaceholder.typicode.com/posts", true)
+    //         xhr.setRequestHeader("content-type", "application/json;charset=UTF-8")
+    //         xhr.onreadystatechange = function () {
+    //         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status >= 200 && xhr.status < 300) {
+    //             alert("message sent")
+    //         } else if (xhr.readyState === XMLHttpRequest.DONE) {
+    //             alert("error")
+    //         }
+    //     }
+    // })
 
             
 
